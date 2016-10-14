a13bed5bf8be54dbbe185cc871c9ee13bb980b8c
Chenxr <Chenxr@DESKTOP-0U3AGA6>
2016-10-14 14:39:55 +0800

    1

--- SouceCode/AgilePointAPI/WorkflowInstance.cs
+++ SouceCode/AgilePointAPI/WorkflowInstance.cs
@@ -16,21 +16,4 @@ namespace AgilePointAPI
         public const string AdministratorAccount = "Administrator";
         public const string AdministratorPassword = "pass";
     }
-
-    public class WorkflowServiceFactory
-    {
-        public static WCFWorkflowProxy CreateWorkflowService(string initiator)
-        {
-            var appName = Constant.AppName;
-            var credential = new NetworkCredential(Constant.AdministratorAccount, Constant.AdministratorPassword, Constant.DomainName);
-            var workFlowServiceBindingName = Convert.ToString(ConfigurationManager.AppSettings["WorkFlowBindingUsed"]);
-            return new WCFWorkflowProxy(appName, string.Empty, "en-US", initiator, credential, workFlowServiceBindingName);
-        }
-    }
-
-    
-
-    
-
-    
 }
