e61d7e67246d86aa45b23f178834739e3f39c663
Chenxr <Chenxr@DESKTOP-0U3AGA6>
2016-10-14 14:36:20 +0800

    1

--- SouceCode/AgilePointAPI/WorkflowInstance.cs
+++ SouceCode/AgilePointAPI/WorkflowInstance.cs
@@ -41,46 +41,7 @@ namespace AgilePointAPI
 
     }
 
-    public class WorkflowInstanceManager : WorkflowManager
-    {
-        public WorkflowInstanceManager(string userAccount) : base(userAccount)
-        {
-        }
-
-        //<summary>
-        // 创建流程实例
-        // defName 流程定义
-        // proInstName 表单名称
-        // atrributes 表单相关字段
-        //submiter 提交人 apdomain\\administrator 必须是域账户
-        //</summary>
-        public void Create(string workflowName, NameValue[] atrributes)
-        {
-            var pid = WorkflowService.GetReleasedPID(workflowName);
-            var piid = UUID.GetID();
-            var workobjectid = UUID.GetID();
-            var instanceTitle = GenerateInstanceTitle(workflowName);
-            var evt = WorkflowService.CreateProcInstEx(pid, piid, instanceTitle, workobjectid, string.Empty, string.Empty, atrributes, true);
-            while (evt.Status == WFEvent.SENT)
-            {
-                System.Threading.Thread.Sleep(1000);
-                evt = WorkflowService.GetEvent(evt.EventID);
-            }
-        }
-
-        // <summary>
-        // 取消流程实例
-        ///// </summary>
-        public void Cancel()
-        {
-
-        }
-
-        private string GenerateInstanceTitle(string workflowName)
-        {
-            return workflowName + DateTime.Now.ToString("yyyyMMddHHmmss");
-        }
-    }
+    
 
     public class WorkflowTaskManager : WorkflowManager
     {
