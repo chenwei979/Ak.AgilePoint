dc9d9e456f792c05fdfd6ae4553a7499368be13d
Chenxr <Chenxr@DESKTOP-0U3AGA6>
2016-10-14 14:53:23 +0800

    1

--- SouceCode/AgilePointAPI/Program.cs
+++ SouceCode/AgilePointAPI/Program.cs
@@ -13,20 +13,9 @@ namespace AgilePointAPI
         static void Main(string[] args)
         {
             WorkflowInstanceManager instance = new WorkflowInstanceManager("apdomain\\administrator");
-
-            //参数
-            //直线领导
             var pars = new List<NameValue>();
             pars.Add(new NameValue("/pd:AP/pd:processFields/pd:Day", 2));
             instance.Create("Lev", pars.ToArray());
         }
-
-        //public string GetBaseProcDefID()
-        //{
-        //    //string URI = "https://mydomain:9011/AgilePointServer/Workflow/GetBaseProcDefID/Test2";
-        //    //HTTPOperations ops = new HTTPOperations("apdomain", "administrator",
-        //    //                     "pass", appID, locale);
-        //    //return ops.GetData(URI);
-        //}
     }
 }
