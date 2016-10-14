256156650499d9704041476d5fd4e12ab0c67c60
Chenxr <Chenxr@DESKTOP-0U3AGA6>
2016-10-14 15:14:34 +0800

    1

--- SouceCode/AgilePointAPI/WorkflowManager.cs
+++ SouceCode/AgilePointAPI/WorkflowManager.cs
@@ -1,4 +1,7 @@
 ﻿using Ascentn.AgilePoint.WCFClient;
+using Ascentn.Workflow.Base;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace AgilePointAPI
 {
@@ -11,6 +14,21 @@ namespace AgilePointAPI
         {
             UserAccount = userAccount;
             WorkflowService = WorkflowServiceFactory.CreateWorkflowService(userAccount);
+        }
+
+        public IEnumerable<NameValue> GenerateAtrributes(object source)
+        {
+            var properties = source.GetType().GetProperties(BindingFlags.DeclaredOnly | BindingFlags.Public);
+            foreach (var property in properties)
+            {
+                var propertyName = GetPropertyName(property.Name);
+                yield return new NameValue(propertyName, property.GetValue(source, null));
+            }
+        }
+
+        public string GetPropertyName(string name, string prefix = "/pd:AP/pd:processFields/pd:")
+        {
+            return string.Concat(prefix, name);
         }
     }
 }
