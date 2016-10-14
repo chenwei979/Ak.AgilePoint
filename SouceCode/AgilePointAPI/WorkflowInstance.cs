6b8ca1113d5ff0113d104341a3a5164bbc00c3ed
Chenxr <Chenxr@DESKTOP-0U3AGA6>
2016-10-14 14:38:28 +0800

    1

--- SouceCode/AgilePointAPI/WorkflowInstance.cs
+++ SouceCode/AgilePointAPI/WorkflowInstance.cs
@@ -28,18 +28,7 @@ namespace AgilePointAPI
         }
     }
 
-    public abstract class WorkflowManager
-    {
-        public string UserAccount { get; private set; }
-        public WCFWorkflowProxy WorkflowService { get; private set; }
-
-        public WorkflowManager(string userAccount)
-        {
-            UserAccount = userAccount;
-            WorkflowService = WCFWorkflowProxyFactory.CreateWorkflowService(userAccount);
-        }
-
-    }
+    
 
     
 
---
+++ SouceCode/AgilePointAPI/WorkflowManager.cs
@@ -0,0 +1,16 @@
+﻿using Ascentn.AgilePoint.WCFClient;
+
+namespace AgilePointAPI
+{
+    public abstract class WorkflowManager
+    {
+        public string UserAccount { get; private set; }
+        public WCFWorkflowProxy WorkflowService { get; private set; }
+
+        public WorkflowManager(string userAccount)
+        {
+            UserAccount = userAccount;
+            WorkflowService = WCFWorkflowProxyFactory.CreateWorkflowService(userAccount);
+        }
+    }
+}
